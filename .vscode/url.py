2e54203bb2a6ca2121501c633f00ac0c600a6ac1
NandyalaSubbaramaiah <nsubbaramaiah96@gmail.com>
2024-06-22 19:10:03 +0530

    02nd_commit

--- .vscode/url.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from geeks_site.views import hello_geecks
-
-urlpatterns=[path('admin/',admin.site.urls), path('geek/',hello_geecks)
-
-]
